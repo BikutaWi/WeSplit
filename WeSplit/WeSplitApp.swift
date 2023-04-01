86526e17182725e3107271ca06ff7476296f4306
William Bikuta <william.bikuta@he-arc.ch>
2023-04-01 15:53:49 +0200

    Add WeSplit project v0.1

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by William Bikuta on 01.04.23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
